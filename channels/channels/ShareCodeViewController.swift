3a187c8d65c9b5fc5e347034c70358f2259ff4b2
Preet <preet@hivepoint.com>
2017-08-17 12:07:16 -0700

    .

---
+++ channels/channels/ShareCodeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ShareCodeViewController.swift
+//  channels
+//
+//  Created by Preet Shihn on 8/17/17.
+//  Copyright © 2017 Hivepoint, Inc. All rights reserved.
+//
+
+import UIKit
+
+class  ShareCodeViewController: UIViewController {
+    
+}
